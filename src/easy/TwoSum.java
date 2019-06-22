e54f6df9558c593961c67d8959bea6f0b3e3335c
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-06-22 12:16:57 +0800

    1.两数之和

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -9,15 +9,22 @@ import java.util.Map;
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-
+        
+        // 新建map
         Map<Integer, Integer> map = new HashMap<>();
+        int temp;
         for (int i = 0; i < nums.length; i++) {
-            int temp = target - nums[i];
+            // 获取差值
+            temp = target - nums[i];
+            // 判断 map 中是否存在差值
             if (map.containsKey(temp)) {
+                // 返回结果
                 return new int[] {map.get(temp), i};
             }
+            // 将该值及索引放入 map 中
             map.put(nums[i], i);
         }
+        // 不存在
         return new int[]{-1, -1};
 
     }
