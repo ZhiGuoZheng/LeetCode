c1e86c2c2b5e2f1e39bade20e890448894fce531
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-06-22 13:22:08 +0800

    7.整数反转

---
+++ src/easy/7.整数反转.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=java
+ *
+ * [7] 整数反转
+ *
+ * https://leetcode-cn.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (32.52%)
+ * Likes:    1098
+ * Dislikes: 0
+ * Total Accepted:    136.4K
+ * Total Submissions: 419.4K
+ * Testcase Example:  '123'
+ *
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 123
+ * 输出: 321
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: -123
+ * 输出: -321
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: 120
+ * 输出: 21
+ * 
+ * 
+ * 注意:
+ * 
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
+ * 0。
+ * 
+ */
+class Solution {
+
+    public int reverse(int x) {
+        long r = 0l;
+        while (x != 0) {
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (r >= Integer.MIN_VALUE && r <= Integer.MAX_VALUE) {
+            return (int)r;
+        } else {
+            return 0;
+        }
+    }
+}
+
